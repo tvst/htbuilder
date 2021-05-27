f4e49ba05c43ae72ee3abfa2e739baa5ff57bf4a
Thiago Teixeira <thiago@streamlit.io>
2021-05-27 16:58:40 -0700

    Up version to 0.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="htbuilder",
-    version="0.4.0",
+    version="0.5.0",
     author="Thiago Teixeira",
     author_email="me@thiagot.com",
     description="A purely-functional HTML builder for Python. Think JSX rather than templates.",
