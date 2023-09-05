7b3233115a176bd0714348ab20c98875ebaa0744
Zachary Blackwood <zachary@streamlit.io>
2023-09-05 10:25:22 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf8") as fh:
 
 setuptools.setup(
     name="htbuilder",
-    version="0.6.1",
+    version="0.6.2",
     author="Thiago Teixeira",
     author_email="me@thiagot.com",
     description="A purely-functional HTML builder for Python. Think JSX rather than templates.",
